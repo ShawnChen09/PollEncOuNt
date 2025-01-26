9123cdb243342dfaa580b9cbc53093b00190737e
Shawn <shawn.for.office@gmail.com>
2025-01-27 01:53:39 +0800

    add a gui

--- src/plc/predict.py
+++ src/plc/predict.py
@@ -11,59 +11,56 @@ def _get_result_count(verbose_results):
 
     return results
 
-def pl_predict(model_path: str,
-            img_dir: str,
-            suffix: str = '.jpg',
-            save_img: bool = True,
-            save_csv: bool = True,
-            save_dir: str = None,
-            csv_name: str = 'results.csv'):
+def pl_predict(img_files: list[str],
+               model_path: str,
+               save_dir: str,
+               save_img: bool = True,
+               save_csv: bool = True,):
     """
     Predicts objects in images using a YOLO model and saves the results to a CSV file.
 
     Args:
+        img_files (list[str]): List of image path to conduct prediction.
         model_path (str): Path to the YOLO model.
-        img_dir (str): Path to the directory containing images.
-        suffix (str, optional): File suffix for images. Defaults to '.jpg'.
+        save_dir (str): Path to the directory to save the results.
         save_img (bool, optional): Whether to save the predicted images. Defaults to True.
         save_csv (bool, optional): Whether to save the results to a CSV file. Defaults to True.
-        save_dir (str, optional): Path to the directory to save the results. Required if save_csv is True.
-        csv_name (str, optional): Name of the CSV file to save the results. Defaults to 'results.csv'.
 
     Returns:
         pandas.DataFrame: DataFrame containing the results.
     """
     assert os.path.isfile(model_path), 'model_path must be a valid file'
-    assert os.path.isdir(img_dir), 'img_dir must be a valid directory'
     if save_dir is not None:
         os.makedirs(save_dir, exist_ok=True)
 
     model = ultralytics.YOLO(model_path)
 
-    files = os.listdir(img_dir)
     df = pd.DataFrame()
-    for file in files:
-        if file.endswith(suffix):
-            path = os.path.join(img_dir, file)
-        else:
-            continue
-        
-        print(f'Predicting {file}...')
+    for file in img_files:
+        print(f'Predicting {file}\n')
 
-        if save_img:
+        if save_img or save_csv:
             assert save_dir is not None and os.path.isdir(save_dir), 'save_dir must be a valid directory'
 
-        res = model.predict(source=path, show=False, save=save_img, conf=0.2, imgsz=1280, show_labels=False, project=save_dir, name="image", exist_ok=True)
+        res = model.predict(source=file,
+                            show=False,
+                            save=save_img,
+                            conf=0.2,
+                            imgsz=1280,
+                            show_labels=False,
+                            project=save_dir,
+                            name="image",
+                            exist_ok=True)
+
         result_count = _get_result_count(res[0].verbose())
         df_extended = pd.DataFrame(result_count, index=[file])
         df = pd.concat([df, df_extended], ignore_index=False)
 
     df = df.fillna(0).astype(int)
     if save_csv:
-        assert save_dir is not None and os.path.isdir(save_dir), 'save_dir must be a valid directory'
-
-        save_path = os.path.join(save_dir, csv_name)
+        save_path = os.path.join(save_dir, "count_result.csv")
         df.to_csv(save_path)
-        print(f'Results saved to {save_path}')
+        print(f'Results saved to: {save_path}\n')
 
+    print("Prediction completed.")
     return df
--- src/plc/train.py
+++ src/plc/train.py
@@ -1,7 +1,7 @@
 import ultralytics
 import os
 
-def pl_train(data_path, save_dir, model_path = "yolov8m.pt", mode = "detect", epochs = 100, device = "cpu"):
+def pl_train(data_path, save_dir, model_path = "yolov8m.pt", epochs = 100, device = "cpu"):
     '''
     Trains a YOLO model using the specified parameters.
 
@@ -9,7 +9,6 @@ def pl_train(data_path, save_dir, model_path = "yolov8m.pt", mode = "detect", ep
         data_path (str): Path to the data .YAML file.
         save_dir (str): Path to the directory to save the trained model.
         model_path (str, optional): Path to the YOLO model. Defaults to "yolov8m.pt".
-        mode (str, optional): Mode for training. Defaults to "detect".
         epochs (int, optional): Number of epochs for training. Defaults to 100.
         device (str, optional): Device to use for training. Defaults to "cpu".
     '''
@@ -19,12 +18,11 @@ def pl_train(data_path, save_dir, model_path = "yolov8m.pt", mode = "detect", ep
     data_path = os.path.abspath(data_path)
     os.makedirs(save_dir, exist_ok=True)
 
-    print("Training started.")
+    print("Training started.\n")
     model = ultralytics.YOLO(model_path)
     model.train(data=data_path,
-                mode=mode,
                 epochs=epochs,
                 device=device,
-                save_dir=save_dir)
-    model.export(format="onnx")
-    print("Training completed.")
+                project=save_dir)
+    # model.export(format="onnx")
+    print("\nTraining completed.")
